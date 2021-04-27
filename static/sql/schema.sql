4f14ed708e6f73e21e21dbe11a951e4ae9970acd
Valerie <vandrade07@yahoo.com>
2021-04-26 22:35:47 -0400

    updated

---
+++ static/sql/schema.sql
@@ -0,0 +1,65 @@
+CREATE TABLE whr2020 (
+	id int PRIMARY KEY,
+	"Country" VARCHAR (75) NOT NULL,
+	"Score" NUMERIC NOT NULL,
+	"Overall Rank" INT NOT NULL,
+	"GDP per Capita" NUMERIC,
+	"Social Support" NUMERIC,
+	"Healthy Life Expectancy" NUMERIC,
+	"Freedom to Make Life Choices" NUMERIC,
+	"Generosity" NUMERIC,
+	"Perceptions of Corruption" NUMERIC
+);
+
+CREATE TABLE whr2019 (
+	id int PRIMARY KEY,
+	"Country" VARCHAR (75) NOT NULL,
+	"Score" NUMERIC NOT NULL,
+	"Overall Rank" INT NOT NULL,
+	"GDP per Capita" NUMERIC,
+	"Social Support" NUMERIC,
+	"Healthy Life Expectancy" NUMERIC,
+	"Freedom to Make Life Choices" NUMERIC,
+	"Generosity" NUMERIC,
+	"Perceptions of Corruption" NUMERIC
+);
+
+CREATE TABLE whr2018 (
+	id int PRIMARY KEY,
+	"Country" VARCHAR (75) NOT NULL,
+	"Score" NUMERIC NOT NULL,
+	"Overall Rank" INT NOT NULL,
+	"GDP per Capita" NUMERIC,
+	"Social Support" NUMERIC,
+	"Healthy Life Expectancy" NUMERIC,
+	"Freedom to Make Life Choices" NUMERIC,
+	"Generosity" NUMERIC,
+	"Perceptions of Corruption" NUMERIC
+);
+
+CREATE TABLE whr2017 (
+	id int PRIMARY KEY,
+	"Country" VARCHAR (75) NOT NULL,
+	"Score" NUMERIC NOT NULL,
+	"Overall Rank" INT NOT NULL,
+	"GDP per Capita" NUMERIC,
+	"Social Support" NUMERIC,
+	"Healthy Life Expectancy" NUMERIC,
+	"Freedom to Make Life Choices" NUMERIC,
+	"Generosity" NUMERIC,
+	"Trust" NUMERIC,
+	"Residual" NUMERIC
+);
+
+CREATE TABLE whr2016 (
+	id int PRIMARY KEY,
+	"Country" VARCHAR (75) NOT NULL,
+	"Score" NUMERIC NOT NULL,
+	"Overall Rank" INT NOT NULL,
+	"GDP per Capita" NUMERIC,
+	"Social Support" NUMERIC,
+	"Healthy Life Expectancy" NUMERIC,
+	"Freedom to Make Life Choices" NUMERIC,
+	"Generosity" NUMERIC,
+	"Trust" NUMERIC
+);
